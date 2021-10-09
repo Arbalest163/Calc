60cfc98e0949aaa4d2910c7b5dc9e49741dc5a45
Arbalest163 <evil_xacker@mail.ru>
2021-10-09 14:59:22 +0400

    Изменения интерфейса

--- Calc/Form1.cs
+++ Calc/Form1.cs
@@ -19,31 +19,31 @@ namespace Calc
 
         private void button1_Click(object sender, EventArgs e)
         {
-            label1.Text += "1";
+            ExpressionLabel.Text += "1";
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            label1.Text += "2";
+            ExpressionLabel.Text += "2";
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            if(label1.Text[label1.Text.Count() - 1] != '+')
-            label1.Text += "+";
+            if(ExpressionLabel.Text[ExpressionLabel.Text.Count() - 1] != '+')
+            ExpressionLabel.Text += "+";
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            if (label1.Text[label1.Text.Count() - 1] != '*')
-                label1.Text += "*";
+            if (ExpressionLabel.Text[ExpressionLabel.Text.Count() - 1] != '*')
+                ExpressionLabel.Text += "*";
         }
 
         private void label1_TextChanged(object sender, EventArgs e)
         {
-            if (label1.Text[label1.Text.Count() - 1] != '*' && label1.Text[label1.Text.Count() - 1] != '+')
+            if (ExpressionLabel.Text[ExpressionLabel.Text.Count() - 1] != '*' && ExpressionLabel.Text[ExpressionLabel.Text.Count() - 1] != '+')
             {
-                label2.Text = (new DataTable().Compute(label1.Text, null)).ToString();
+                label2.Text = "= " + (new DataTable().Compute(ExpressionLabel.Text, null)).ToString();
             }
         }
     }
